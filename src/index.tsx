6de04fa8eb99fadb092d67e9f333da0e137b4d47
happyhyep <jhi2359@naver.com>
2024-01-12 18:08:15 +0900

    recoil 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "src/lang/index";
+import { RecoilRoot } from "recoil";
 
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <RecoilRoot>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </RecoilRoot>
+  </>,
 );
