b28f2c323871e122b9abcc16404612d48a352324
happyhyep <jhi2359@naver.com>
2024-01-15 13:06:51 +0900

    skills 컴포넌트 스타일 수정

--- src/components/SkillsContainer.tsx
+++ src/components/SkillsContainer.tsx
@@ -2,94 +2,40 @@ import React from "react";
 
 const SkillsContainer = () => {
   return (
-    <>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+    <section className="skills-container">
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/HTML5-E34F26?style=flat&logo=HTML5&logoColor=white" />
         <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat&logo=JavaScript&logoColor=black" />
         <img src="https://img.shields.io/badge/TypeScript-3178C6?style=flat&logo=TypeScript&logoColor=white" />
         <img src="https://img.shields.io/badge/React-61DAFB?style=flat&logo=React&logoColor=black" />
         <img src="https://img.shields.io/badge/Vue-4FC08D?style=flat&logo=Vue.js&logoColor=black" />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/Node.js-339933?style=flat&logo=Node.js&logoColor=white" />
         <img src="https://img.shields.io/badge/Python-3776AB?style=flat&logo=Python&logoColor=white" />
         <img src="https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white" />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/PostgreSQL-4169E1?style=flat&logo=PostgreSQL&logoColor=black" />
         <img src="https://img.shields.io/badge/Firebase-FFCA28?style=flat&logo=firebase&logoColor=black" />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/Docker-2496ED?style=flat&logo=Docker&logoColor=white" />
         <img src="https://img.shields.io/badge/Kubernetes-326CE5?style=flat&logo=Docker&logoColor=white" />
         <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat&logo=amazonaws&logoColor=white" />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/figma-F24E1E?style=flat&logo=figma&logoColor=white" />
         <img src="https://img.shields.io/badge/styledComponents-DB7093?style=flat&logo=styled-components&logoColor=white" />
         <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=CSS3&logoColor=white" />
         <img src="https://img.shields.io/badge/Sass-CC6699?style=flat&logo=Sass&logoColor=white" />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          gap: "0.6rem",
-          margin: "auto",
-          marginTop: "1.25rem",
-          marginBottom: "1.25rem",
-        }}
-      >
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/Adobe Photoshop-31A8FF?style=flat&logo=Adobe Photoshop&logoColor=black" />
         <img src="https://img.shields.io/badge/Adobe Premiere Pro-9999FF?style=flat&logo=Adobe Premiere Pro&logoColor=black" />
         <img src="https://img.shields.io/badge/Adobe After Effects-9999FF?style=flat&logo=Adobe After Effects&logoColor=black" />
       </div>
-    </>
+    </section>
   );
 };
 
