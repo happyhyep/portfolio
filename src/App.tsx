670a620f203acebcb8a3d83637ce152e9dcb1953
happyhyep <jhi2359@naver.com>
2024-02-02 20:26:10 +0900

    github 배포시 라우터 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect } from "react";
 import "./assets/css/app.scss";
 import Router from "./router/index";
 import { preloadImage } from "src/lib/worker/preloadImage";
