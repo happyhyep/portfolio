42d111ce24d30aef3b8990c8988d8947606c3fa2
happyhyep <jhi2359@naver.com>
2024-02-21 21:09:27 +0900

    modify skills

--- src/components/SkillsContainer.tsx
+++ src/components/SkillsContainer.tsx
@@ -11,6 +11,14 @@ const SkillsContainer = () => {
         <img src="https://img.shields.io/badge/Vue-4FC08D?style=flat&logo=Vue.js&logoColor=black" />
       </div>
       <div className="skills-wrapper">
+        <img src="https://img.shields.io/badge/Recoil-3578E5?style=flat&logo=Recoil&logoColor=white" />
+        <img src="https://img.shields.io/badge/Pinia-F2E142?style=flat&logo=Vue.js&logoColor=white" />
+        <img src="https://img.shields.io/badge/i18Next-26A69A?style=flat&logo=i18Next&logoColor=white" />
+        <img src="https://img.shields.io/badge/VueI18n-4FC08D?style=flat&logo=Vue.js&logoColor=white" />
+        <img src="https://img.shields.io/badge/Prettier-F7B93E?style=flat&logo=prettier&logoColor=white" />
+        <img src="https://img.shields.io/badge/Eslint-4B32C3?style=flat&logo=eslint&logoColor=white" />
+      </div>
+      <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/Node.js-339933?style=flat&logo=Node.js&logoColor=white" />
         <img src="https://img.shields.io/badge/Python-3776AB?style=flat&logo=Python&logoColor=white" />
         <img src="https://img.shields.io/badge/C++-00599C?style=flat&logo=cplusplus&logoColor=white" />
@@ -21,11 +29,15 @@ const SkillsContainer = () => {
       </div>
       <div className="skills-wrapper">
         <img src="https://img.shields.io/badge/Docker-2496ED?style=flat&logo=Docker&logoColor=white" />
-        <img src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat&logo=amazonaws&logoColor=white" />
+        <img src="https://img.shields.io/badge/Docker compose-2496ED?style=flat&logo=Docker&logoColor=white" />
+        <img src="https://img.shields.io/badge/Amazon ec2-FF9900?style=flat&logo=amazonec2&logoColor=white" />
+        <img src="https://img.shields.io/badge/Amazon s3-569A31?style=flat&logo=amazons3&logoColor=white" />
+        <img src="https://img.shields.io/badge/Nginx-009639?style=flat&logo=nginx&logoColor=white" />
       </div>
+      <br />
       <div className="skills-wrapper">
-        <img src="https://img.shields.io/badge/figma-F24E1E?style=flat&logo=figma&logoColor=white" />
-        <img src="https://img.shields.io/badge/styledComponents-DB7093?style=flat&logo=styled-components&logoColor=white" />
+        <img src="https://img.shields.io/badge/Figma-F24E1E?style=flat&logo=figma&logoColor=white" />
+        <img src="https://img.shields.io/badge/Styled Components-DB7093?style=flat&logo=styled-components&logoColor=white" />
         <img src="https://img.shields.io/badge/CSS3-1572B6?style=flat&logo=CSS3&logoColor=white" />
         <img src="https://img.shields.io/badge/Sass-CC6699?style=flat&logo=Sass&logoColor=white" />
       </div>
