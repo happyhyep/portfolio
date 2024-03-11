e0f2dc1b1af4b04bc755d3d0dc5db03f1a3d1c2d
jeong hyein <hyen@happyhyep.local>
2024-03-11 16:53:40 +0900

    prettier setting

--- src/components/common/PageGuideComponent.tsx
+++ src/components/common/PageGuideComponent.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const PageGuideComponent = () => {
-  return <section className="page-guide-component"></section>;
+  return (
+    <section className="page-guide-component">
+      <img
+        src={process.env.PUBLIC_URL + "/assets/icons/guide/arrow-left.svg"}
+      />
+    </section>
+  );
 };
 
 export default PageGuideComponent;
