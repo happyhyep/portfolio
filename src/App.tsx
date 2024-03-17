30d38428bb28034aa54442a4113c16f946ca18b9
hyen <jhi2359@naver.com>
2024-03-17 19:56:49 +0900

    초기 언어 한국어로 저장

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import React, { useEffect } from "react";
 import "./assets/css/app.scss";
 import Router from "./router/index";
 import { preloadImage } from "src/lib/worker/preloadImage";
+import { saveLocalData } from "src/utils/common-util";
+import AppConfig from "src/constants";
 
 function App() {
   let page = "";
 
+  useEffect(() => {
+    saveLocalData(AppConfig.KEYS.LANG, "ko");
+  }, []);
   useEffect(() => {
     if (window.location.href.includes("introduce")) {
       page = "IntroducePage";
